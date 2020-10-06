b8a68db840146f8816edf96b46f6ee89a7b74060
Magdalena Marchewka <madilenaa@gmail.com>
2020-10-06 18:34:07 +0200

    add routing, redux and filters

---
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux';
+import TripListOptions from './TripListOptions';
+import {getAllTags} from '../../../redux/tagsRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, addTag, removeTag} from '../../../redux/filtersRedux';
+
+const mapStateToProps = state => ({
+  tags: getAllTags(state),
+  filters: getAllFilters(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  changeDuration: duration => dispatch(changeDuration(duration)),
+  addTag: tag => dispatch(addTag(tag)),
+  removeTag: tag => dispatch(removeTag(tag)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
---
+++ src/redux/filtersRedux.js
@@ -0,0 +1,53 @@
+/* SELECTORS */
+
+export const getAllFilters = ({filters}) => filters;
+
+/* ACTIONS */
+
+// action name creator
+const reducerName = 'filters';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// action types
+export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
+
+// action creators
+export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+export const changeDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const addTag = payload => ({ payload, type: ADD_TAG });
+export const removeTag = payload => ({ payload, type: REMOVE_TAG });
+
+// reducer
+export default function reducer(statePart = [], action = {}) {
+  switch (action.type) {
+    case CHANGE_PHRASE: {
+      return {
+        ...statePart,
+        searchPhrase: action.payload,
+      };
+    }
+    case CHANGE_DURATION: {
+      return {
+        ...statePart,
+        duration: action.payload,
+      };
+    }
+    case ADD_TAG: {
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload.tag],
+      };
+    }
+    case REMOVE_TAG: {
+      return {
+        ...statePart,
+        tags: statePart.tags.filter(tag => tag != action.payload.tag),
+      };
+    }
+    default:
+      return statePart;
+  }
+}
