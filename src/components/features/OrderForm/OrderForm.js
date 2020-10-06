97488f49a1c3cff730a07cb49b03e397db46348b
Magdalena Marchewka <madilenaa@gmail.com>
2020-10-06 23:02:27 +0200

    add order price

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Grid, Row, Col} from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+//import styles from './OrderForm.scss';
+
+const OrderForm = ({tripCost, options}) => (
+  <Grid>
+    <Row>
+      <Col xs={12}>
+        <OrderSummary tripCost={tripCost} options={options} />
+      </Col>
+    </Row>
+  </Grid>
+);
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderForm;
